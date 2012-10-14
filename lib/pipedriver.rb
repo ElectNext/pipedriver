d6a33486152126b98b82a57e8387afb853d513b0
Lachy Groom <lachygroom@gmail.com>
2012-10-13 17:59:56 -0700

    attempt at fixing requirement bug

--- lib/pipedriver.rb
+++ lib/pipedriver.rb
@@ -1,6 +1,7 @@
 require_relative 'pipedriver/version'
 require_relative 'pipedriver/configuration'
 require_relative 'pipedriver/client'
+require_relative 'pipedriver/deals'
 
 module Pipedriver
   extend Configuration
